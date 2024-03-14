1d692540e3bd4fe2a2624da6fa074e3886fae67f
Lukman Sanusi <lckmnzans@gmail.com>
2024-03-14 20:30:59 +0700

    implementasi mainmenu selesai

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,21 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     private static final Scanner inputMenu = new Scanner(System.in);
     public static void main(String[] args) {
+        System.out.println(
+                "=".repeat(30) + "\n Selamat datang di Binar Food \n" + "=".repeat(30)
+        );
         mainMenu();
     }
 
     public static void mainMenu() {
-        System.out.println(
-                "=".repeat(30) + "\n Selamat datang di Binar Food \n" + "=".repeat(30)
-        );
+        List<String> menuList = Arrays.asList("1","2","3","4","5","6","7","8");
+        List<String> menuOption = Arrays.asList("00","01");
         System.out.println(
                 """
                         Silahkan pilih menu makanan minuman:\s
@@ -25,15 +27,22 @@ public class Main {
                         6. Orak-arik Telur          |  8.000
                         7. Es Teh                   |  3.000
                         8. Es Jeruk                 |  3.000
+                        
                         00. Pesan dan bayar
                         01. Keluar dan batalkan"""
         );
-        System.out.print("=> ");
-        String userInput = inputMenu.nextLine();
-        selectedMenu(userInput);
+        System.out.print("=> ");String userInput = inputMenu.nextLine();
+        if (menuList.contains(userInput)) {
+            selectedMenu(userInput);
+        } else if (menuOption.contains(userInput)) {
+            subMenu(userInput);
+        } else {
+            System.out.println("Maaf, masukkan menu atau pilihan yang sesuai! \n" + "=".repeat(30));
+            mainMenu();
+        }
     }
 
-    private static void subMenu() {
+    private static void subMenu(String menu) {
         //block
     }
 
@@ -50,8 +59,15 @@ public class Main {
             default -> "";
         };
         String[] item = new String[8];
-        System.out.println("=".repeat(30) + "\nSilahkan masukkan jumlah makanan/minuman \n + " + selectedItem);
-        System.out.println("qty => ");String userInput = inputMenu.nextLine();
-        System.out.println("Jumlah yang anda pesan "+ userInput);
+        StringBuilder inputMsg = new StringBuilder();
+        inputMsg.append("=".repeat(30))
+                .append("\n")
+                .append("Silahkan masukkan jumlah makanan/minuman \n")
+                .append("+")
+                .append(selectedItem);
+        System.out.println(inputMsg);
+        System.out.print("qty => ");String userInput = inputMenu.nextLine();
+        item[Integer.parseInt(menu) - 1] = userInput;
+        System.out.println("Jumlah yang anda pesan untuk menu "+ selectedItem +" adalah " + userInput + " pesanan");
     }
 }
