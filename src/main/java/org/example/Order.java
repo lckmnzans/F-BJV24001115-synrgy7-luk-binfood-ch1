43b407b542ba486a821373da3da938d6088bfb7f
Lukman Sanusi <lckmnzans@gmail.com>
2024-03-16 03:02:03 +0700

    fungsi cetak struk selesai

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -1,12 +1,15 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 import static org.example.Const.menuPrice;
 
 public class Order {
     private static Order order;
-    private int[][] orderedMenu = new int[2][8];
+    private final int[][] orderedMenu = new int[2][8];
+    public final static String PRICE = "price";
+    public final static String QTY = "qty";
 
     public static Order getInstance() {
         if (order == null) {
@@ -15,18 +18,29 @@ public class Order {
         return order;
     }
 
-    public void addOrder(int menuId, int qty) {
+    public void addOrder(String menu, int qty) {
+        int menuId = Integer.parseInt(menu);
         orderedMenu[0][menuId-1] = qty;
         orderedMenu[1][menuId-1] = menuPrice[menuId-1] * qty;
         System.out.println("Menu berhasil ditambahkan ke daftar pesanan anda");
     }
 
-    public int[][] takeOrder() {
+    public int[][] getOrders() {
         return orderedMenu;
+    }
+
+    public int getOrder(String menu, String lookup) {
+        int menuId = Integer.parseInt(menu);
+        if (Objects.equals(lookup, PRICE)) {
+            return orderedMenu[1][menuId-1];
+        } else {
+            return orderedMenu[0][menuId-1];
+        }
     }
 
     public void clearOrder() {
         Arrays.fill(orderedMenu, 0);
     }
+
 
 }
